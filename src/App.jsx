09360e3c78164ab721e14541cc45fc9d48a254aa
Mayank Sharma <102649566+MayankS1h3@users.noreply.github.com>
2025-07-29 00:01:51 +0530

    Initial setup with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className="w-full max-w-md mx-auto shadow-md-rounded-lg px-4 my-8 text-lime-700 bg-amber-100">
+          password
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
